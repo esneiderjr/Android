f62b5539faebc1b87c1a3898eede398dbea76abc
Esneiderjr <esneidernorena08@gmail.com>
2022-04-04 17:48:22 -0500

    reports

---
+++ mobile/lib/Reports.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Reports extends StatelessWidget {
+  const Reports({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text('Klotthy'),
+      ),
+    );
+  }
+}
