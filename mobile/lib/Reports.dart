958fdffa06e7979afff18f2e2ddbe5f825f0e013
Esneiderjr <esneidernorena08@gmail.com>
2022-04-04 23:53:10 -0500

    botones

--- mobile/lib/Reports.dart
+++ mobile/lib/Reports.dart
@@ -11,16 +11,34 @@ class _ReportsState extends State<Reports> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          title: const Text('Clotthy', style: TextStyle(color: Color.fromARGB(255, 0, 0, 0))),
+      backgroundColor: Color.fromARGB(250, 252, 255, 253),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Text('Clotthy',
+            style: TextStyle(color: Color.fromARGB(255, 0, 0, 0))),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          ButtonBar(
+            children: <Widget>[],
+          );
+        },
+        child: Icon(Icons.menu_outlined),
+      ),
+      body: Center(
+        child: Container(
+          color: Colors.black.withAlpha(25),
+          width: 280,
+          height: 40,
+          child: ElevatedButton(
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(
+                    Color.fromARGB(255, 36, 91, 189))),
+            onPressed: () {},
+            child: Text('Generar reporte'),
+          ),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => Reports()));
-          },
-          child: Icon(Icons.abc_outlined),
-        ));
+      ),
+    );
   }
 }
