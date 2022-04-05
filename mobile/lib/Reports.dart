af55b6a550cd762ff7287dad39ff5fca0b2b4251
Esneiderjr <esneidernorena08@gmail.com>
2022-04-04 19:26:39 -0500

    delete

--- mobile/lib/Reports.dart
+++ mobile/lib/Reports.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
 
-class Reports extends StatelessWidget {
+class Reports extends StatefulWidget {
   const Reports({Key? key}) : super(key: key);
 
   @override
+  State<Reports> createState() => _ReportsState();
+}
+
+class _ReportsState extends State<Reports> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Text('Klotthy'),
-      ),
-    );
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: const Text('Klotthy', style: TextStyle(color: Colors.black)),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => Reports()));
+          },
+          child: Icon(Icons.abc_outlined),
+        ));
   }
 }
