140b428edcfb56b099a1cf468b20a2c2a6c6e6a4
Esneiderjr <esneidernorena08@gmail.com>
2022-04-07 00:12:46 -0500

    date

--- mobile/lib/Reports.dart
+++ mobile/lib/Reports.dart
@@ -11,6 +11,28 @@ class Reports extends StatefulWidget {
 class _ReportsState extends State<Reports> {
   @override
   Widget build(BuildContext context) {
+
+     Future <DateTime?> getDatePickerWidget() {
+      return showDatePicker(
+          context: context,
+          initialDate: DateTime.now(),
+          firstDate: DateTime(2020),
+          lastDate: DateTime(2022),
+          builder: (context, child) {
+            return Theme(data: ThemeData.light(), child:widget);
+          });
+    }
+
+    var _currentSelectedDate;
+    var selectDate;
+    void callDatePicker() async {
+      var selectDate = await getDatePickerWidget();
+      setState(() {
+        _currentSelectedDate = selectDate;
+      });
+    }
+
+   
     return Scaffold(
       backgroundColor: Color.fromARGB(250, 252, 255, 253),
       appBar: AppBar(
@@ -28,44 +50,57 @@ class _ReportsState extends State<Reports> {
       ),
       body:
           // este es el boton para generar reportes
-          Container(
-        width: 220,
-        height: 40,
-        child: ElevatedButton(
-          style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all<Color>(
-                  Color.fromARGB(255, 36, 91, 189))),
-          onPressed: () {
-            // el AlertDialog para poder elegir la fecha del reporte
-            AlertDialog alert = AlertDialog(
-              title: const Text(
-                'GENERAR REPORTE',
-                // style: GoogleFonts.sourceSansPro(),
-                textAlign: TextAlign.center,
-              ),
-              content: Container(
-                height: 320,
-                width: 210,
-                child: Column(
-                  children: [Text('')],
+          Center(
+        child: Container(
+          width: 300,
+          height: 40,
+          child: ElevatedButton(
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(
+                    Color.fromARGB(255, 36, 91, 189))),
+            onPressed: () {
+              // el AlertDialog para poder elegir la fecha del reporte
+              AlertDialog alert = AlertDialog(
+                title: const Text(
+                  'GENERAR REPORTE',
+                  // style: GoogleFonts.sourceSansPro(),
+                  textAlign: TextAlign.center,
                 ),
-              ),
-              actions: <Widget>[
-                TextButton(
-                  child: const Text('OK'),
-                  onPressed: () {
-                    Navigator.pop(context, 'OK');
-                  },
+                content: Container(
+                  height: 320,
+                  width: 210,
+                  child: Column(
+                    children: [
+                      Text(
+                        'Fecha inicial',
+                        textAlign: TextAlign.justify,
+                      ),
+                      
+                      Column(
+                        children: [
+                          
+                        ],
+                      )
+                    ],
+                  ),
                 ),
-              ],
-            );
-            showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return alert;
-                });
-          },
-          child: Text('Generar reporte'),
+                actions: <Widget>[
+                  TextButton(
+                    child: const Text('OK'),
+                    onPressed: () {
+                      Navigator.pop(context, 'OK');
+                    },
+                  ),
+                ],
+              );
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return alert;
+                  });
+            },
+            child: Text('Generar reporte'),
+          ),
         ),
       ),
     );
