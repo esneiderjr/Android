bcb12fad389c0a0a09732aa566f4eb68b470419f
Esneiderjr <esneidernorena08@gmail.com>
2022-04-07 02:05:33 -0500

    hello

--- mobile/lib/Reports.dart
+++ mobile/lib/Reports.dart
@@ -11,99 +11,101 @@ class Reports extends StatefulWidget {
 class _ReportsState extends State<Reports> {
   @override
   Widget build(BuildContext context) {
-    Future<DateTime?> getDatePickerWidget() {
-      return showDatePicker(
-          context: context,
-          initialDate: DateTime.now(),
-          firstDate: DateTime(2020),
-          lastDate: DateTime(2022),
-          builder: (context, child) {
-            return Theme(data: ThemeData.light(), child: widget);
-          });
-    }
-
-    var _currentSelectedDate;
-    var selectDate;
-    void callDatePicker() async {
-      var selectDate = await getDatePickerWidget();
-      setState(() {
-        _currentSelectedDate = selectDate;
-      });
-    }
-
     return Scaffold(
-        backgroundColor: Color.fromARGB(250, 252, 255, 253),
-        appBar: AppBar(
-            backgroundColor: Colors.white,
-            titleSpacing: d,
-            ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            ButtonBar(
-              children: <Widget>[],
-            );
-          },
-          child: Icon(Icons.menu_outlined),
+      backgroundColor: const Color.fromARGB(250, 252, 255, 253),
+      appBar: AppBar(
+        leading: Container(
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+            image: AssetImage('images/clotthy.png'),
+            fit: BoxFit.contain,
+          )),
         ),
-        body:
-            // este es el boton para generar reportes
-            Center(
-          child: Container(
-            decoration: BoxDecoration(
-                image: DecorationImage(
-              image: AssetImage('images/clotthy.png'),
-              fit: BoxFit.contain,
-            )),
-            child: ElevatedButton(
-              style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(
-                      Color.fromARGB(255, 36, 91, 189))),
-              onPressed: () {
-                // el AlertDialog pbaara poder elegir la fecha del reporte
-                AlertDialog alert = AlertDialog(
-                  title: const Text(
-                    'GENERAR REPORTE',
-                    // style: GoogleFonts.sourceSansPro(),
-                    textAlign: TextAlign.center,
+        backgroundColor: Colors.white,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          const ButtonBar(
+            children: <Widget>[],
+          );
+        },
+        child: Icon(Icons.menu_outlined),
+      ),
+      body: Column(children: [
+        // este es el boton para generar reportes
+        Divider(color: Colors.white.withOpacity(0.1)),
+        Center(
+            child: Container(
+          height: 40,
+          width: 250,
+          child: ElevatedButton(
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(
+                    Color.fromARGB(255, 36, 91, 189))),
+            onPressed: () {
+              // el AlertDialog pbaara poder elegir la fecha del reporte
+              AlertDialog alert = AlertDialog(
+                title: const Text(
+                  'GENERAR REPORTE',
+                  textAlign: TextAlign.center,
+                ),
+                content: Container(
+                  height: 320,
+                  width: 210,
+                  child: Column(
+                    children: [
+                      const Text(
+                        'Fecha inicial',
+                        textAlign: TextAlign.justify,
+                      ),
+                      Column(
+                        children: [
+                          ElevatedButton(
+                              style: ButtonStyle(),
+                              onPressed: () {},
+                              child: widget)
+                        ],
+                      )
+                    ],
                   ),
-                  content: Container(
-                    height: 320,
-                    width: 210,
-                    child: Column(
-                      children: [
-                        Text(
-                          'Fecha inicial',
-                          textAlign: TextAlign.justify,
-                        ),
-                        Column(
-                          children: [
-                            ElevatedButton(
-                                style: ButtonStyle(),
-                                onPressed: () {},
-                                child: widget)
-                          ],
-                        )
-                      ],
-                    ),
+                ),
+                actions: <Widget>[
+                  TextButton(
+                    child: const Text('OK'),
+                    onPressed: () {
+                      Navigator.pop(context, 'OK');
+                    },
                   ),
-                  actions: <Widget>[
-                    TextButton(
-                      child: const Text('OK'),
-                      onPressed: () {
-                        Navigator.pop(context, 'OK');
-                      },
-                    ),
-                  ],
-                );
-                showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return alert;
-                    });
-              },
-              child: Text('Generar reporte'),
-            ),
+                ],
+              );
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return alert;
+                  });
+            },
+            child: const Text('Generar reporte'),
           ),
-        ));
+        )),
+        Divider(color: Colors.white.withOpacity(0.1)),
+        Container(
+            width: 300,
+            alignment: Alignment.bottomLeft,
+            child: const Text(
+              'Reportes',
+              textAlign: TextAlign.left,
+              style: TextStyle(fontSize: 20),
+            )),
+        // imagen de fondo
+        Center(
+            child: Container(
+                height: 400,
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                  image: AssetImage('images/background_decoration.png'),
+                  fit: BoxFit.contain,
+                )))),
+      ]),
+    );
   }
 }
