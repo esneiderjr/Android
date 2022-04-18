d5d626af90e930e624e2bc4d0fcbde792d738bcb
Esneiderjr <esneidernorena08@gmail.com>
2022-04-18 17:34:43 -0500

    update

--- mobile/lib/Login/screens/Reports.dart
+++ mobile/lib/Login/screens/Reports.dart
@@ -8,7 +8,6 @@ class Reports extends StatefulWidget {
   @override
   State<Reports> createState() => _ReportsState();
 }
-
 class _ReportsState extends State<Reports> {
   @override
   Widget build(BuildContext context) {
