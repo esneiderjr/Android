c4536ccbf34f0547c8450d786929e08b9347c526
Esneiderjr <esneidernorena08@gmail.com>
2022-04-18 18:41:21 -0500

    update

--- mobile/lib/Login/Widgets/auth_background.dart
+++ mobile/lib/Login/Widgets/auth_background.dart
@@ -3,10 +3,7 @@ import 'package:flutter/material.dart';
 class AuthBackground extends StatelessWidget {
   final Widget child;
 
-  const AuthBackground({
-    Key? key,
-    required this.child
-    }) : super(key: key);
+  const AuthBackground({Key? key, required this.child}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -33,68 +30,62 @@ class _HeaderImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-       child: 
-      Flexible(
+      child: Flexible(
         fit: FlexFit.loose,
-      child: Container(
-        width: double.infinity,
-        margin: EdgeInsets.only(top: 30),
-        child: Image.asset('images/clotthy.png',height:135,
-         ),
-      ),
+        child: Container(
+          width: double.infinity,
+          margin: EdgeInsets.only(top: 30),
+          child: Image.asset(
+            'images/clotthy.png',
+            height: 135,
+          ),
+        ),
       ),
     );
   }
 }
 
 class _CajaColor extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-
     final size = MediaQuery.of(context).size;
 
     return Container(
-      width: double.infinity,
-      height: size.height * 0.6,
-      decoration: _bluebackground(),
-      child: Stack(
-        children: [
-          Positioned(child: _Buble(), top: 90, left: 30),
-          Positioned(child: _Buble(), top: -40, left: -30),
-          Positioned(child: _Buble(), top: -50, right: -20),
-          Positioned(child: _Buble(), bottom: -50, left: 10),
-          Positioned(child: _Buble(), bottom: 120, right: 20),
-        ],
-      )
-    );
+        width: double.infinity,
+        height: size.height * 0.6,
+        decoration: _bluebackground(),
+        child: Stack(
+          children: [
+            Positioned(child: _Buble(), top: 90, left: 30),
+            Positioned(child: _Buble(), top: -40, left: -30),
+            Positioned(child: _Buble(), top: -50, right: -20),
+            Positioned(child: _Buble(), bottom: -50, left: 10),
+            Positioned(child: _Buble(), bottom: 120, right: 20),
+          ],
+        ));
   }
 
   BoxDecoration _bluebackground() => BoxDecoration(
-    borderRadius: BorderRadius.only(
-      bottomRight: Radius.circular(100),
-      bottomLeft: Radius.circular(100),
-    ),
-    gradient: LinearGradient(colors: [
-      Color.fromARGB(255, 36, 91, 189),
-      Color.fromARGB(255, 59, 158, 240),
-    ]
-    ),
-  );
+        borderRadius: BorderRadius.only(
+          bottomRight: Radius.circular(100),
+          bottomLeft: Radius.circular(100),
+        ),
+        gradient: LinearGradient(colors: [
+          Color.fromARGB(255, 36, 91, 189),
+          Color.fromARGB(255, 36, 91, 189)
+        ]),
+      );
 }
 
-
 class _Buble extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 100,
-      height: 100,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(100),
-        color: Color.fromRGBO(255, 255, 255, 0.05),
-      )
-    );
+        width: 100,
+        height: 100,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100),
+          color: Color.fromRGBO(255, 255, 255, 0.05),
+        ));
   }
 }
