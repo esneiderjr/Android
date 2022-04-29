f9be2674a3926f3e805d2548b1306c968180aa9f
esneiderjr <esneidernorena08@gmail.com>
2022-04-29 09:11:08 -0500

    appBar

---
+++ mobile/lib/appBar/appBar.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class appBar extends StatelessWidget {
+  const appBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return appBar(
+      
+    );
+  }
+}
