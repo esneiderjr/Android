99f34416079c1f961d90ad619fbaa6a03e01c2c4
Esneiderjr <esneidernorena08@gmail.com>
2022-05-05 20:49:15 -0500

    update appBar

--- mobile/lib/appBar/appBar.dart
+++ mobile/lib/appBar/appBar.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 
 class CustomAppBar extends StatelessWidget with PreferredSizeWidget {
@@ -11,15 +14,46 @@ class CustomAppBar extends StatelessWidget with PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
+      automaticallyImplyLeading: false,
       backgroundColor: const Color.fromARGB(250, 252, 255, 253),
-      leading: Container(
-        width: 60,
-        height: 70,
-        decoration: const BoxDecoration(
-            image: DecorationImage(
-          image: AssetImage('images/clotthy2.png'),
-        )),
+      title: Row(
+        children: [
+          ZoomIn(
+              child: Image.asset(
+            'images/clotthy2.png',
+            fit: BoxFit.contain,
+            height: 48,
+          )),
+        ],
       ),
+      actions: [
+        Container(
+          child: Column(
+            children: [ 
+              const Divider(
+                height: 10,
+              ),
+              Text(
+                'Alberto Carrascales',
+                style:
+                    TextStyle(color: Colors.black, fontFamily: 'SourceSansPro'),
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                'Anlista',
+                textAlign: TextAlign.right,
+                style: TextStyle(color: Colors.grey, fontSize: 10),
+              )
+            ],
+          ),
+        ),
+        CircleAvatar(
+          child: Image.asset(
+            'images/Perfil.png',
+            fit: BoxFit.contain,
+          ),
+        ),
+      ],
     );
   }
 }
