ac8b9bbd0d070136029ded16361785728124a14f
Stevlon <joanlondono2001@gmail.com>
2022-06-20 19:24:53 -0500

    .

--- mobile/lib/Login/screens/login_screen.dart
+++ mobile/lib/Login/screens/login_screen.dart
@@ -182,8 +182,8 @@ class _LoginForm extends State<LoginForm> {
                     ),
                     onPressed: () {
                       // llama al provider para conectarlo con la api
-                      // provider.getUsuario(
-                      //     loginform.email, loginform.password, context);
+                      provider.getUsuario(
+                          loginform.email, loginform.password, context);
                       // todo login form
                       String sinspa = loginform.email.replaceAll(" ", "");
                       if (!loginform.isValidForm()) return;
